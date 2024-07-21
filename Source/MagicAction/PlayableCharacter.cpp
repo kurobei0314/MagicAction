05c2a788af82cd33da37f0299b1a0bac94896bcd
kurobei0314 <maikurodei.biseibutu@gmail.com>
2024-07-21 14:53:41 +0900

    [refactor] 関係ない箇所を削除

--- Source/MagicAction/PlayableCharacter.cpp
+++ Source/MagicAction/PlayableCharacter.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayableCharacter.h"
-#include "Components/StaticMeshComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Components/InputComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -13,11 +12,9 @@
 // Sets default values
 APlayableCharacter::APlayableCharacter()
 {
-	UE_LOG(LogTemp, Warning, TEXT("start"));
 	PrimaryActorTick.bCanEverTick = true;
 	ActionInput = LoadObject<UInputAction>(NULL, TEXT("/Game/Inputs/IA_Move"), NULL, LOAD_None, NULL);
 	DefaultMappingContext = LoadObject<UInputMappingContext>(NULL, TEXT("/Game/Inputs/IMC_Default"), NULL, LOAD_None, NULL);
-	Mesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Game/Character/Textures_hw/unitychan_hw"));
 }
 
 // Called when the game starts or when spawned
@@ -39,7 +36,6 @@ void APlayableCharacter::BeginPlay()
 
 void APlayableCharacter::MovePlayerAction()
 {
-	// UKismetSystemLibrary::PrintString(this, TEXT("W"), true, true, FColor::Cyan, 10.0f, TEXT("None"));
 	UE_LOG(LogTemp, Warning, TEXT("W"));
 }
 
